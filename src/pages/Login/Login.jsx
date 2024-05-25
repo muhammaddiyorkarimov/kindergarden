d7fa7c9c06b92fb7d3f3b2c4d128f54440489596
Muhammaddiyor <muhammaddiyor1516@gmail.com>
2024-05-26 00:07:00 +0500

    m

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
