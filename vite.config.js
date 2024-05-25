eb3ebab5828197301c8fe2b9ec7a0dd6bc027415
Muhammaddiyor <muhammaddiyor1516@gmail.com>
2024-05-25 23:25:35 +0500

    m

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// vite.config.ts
-// vite.config.js
-
 export default defineConfig({
-  // boshqa konfiguratsiyalar
+  plugins: [react()],
   server: {
     port: 8001
   }
